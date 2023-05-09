e4af1b3cda270e0e77830dabb972dd21e80827bc
alif-dot <alif17648@gmail.com>
2023-05-09 21:22:37 +0600

    Add project files.

---
+++ ProductLinkQ/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLinkQ
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public Category(int categoryID, string name) : this()
+        {
+            CategoryID = categoryID;
+            CategoryName = name;
+        }
+    }
+}
---
+++ ProductLinkQ/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLinkQ
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int Quentity { get; set; }
+        public string ProductName { get; set; }
+        public int ProductNumber { get; set; }
+        public string Color { get; set; }
+        public double FixedPrice { get; set; }
+        public int CategoryID { get; set; }
+        public int ModelID { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ProductModel Model { get; set; }
+
+        public Product()
+        {
+
+        }
+        
+        public Product(int productID, string name, double price, int quentity, int productNumber, int categoryID, int modelID, string color) : this()
+        {
+            ProductId = productID;
+            Quentity = quentity;
+            ProductName = name;
+            FixedPrice = price;
+            CategoryID = categoryID;
+            ModelID = modelID;
+            Color = color;
+        }
+    }
+}
